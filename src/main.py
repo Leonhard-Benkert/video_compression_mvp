211d62f1bef41286af69f3bff260a7c019391955
leonhardbenkert <ge96cej@mytum.de>
2022-10-21 08:44:59 +0200

    Add script files

---
+++ src/main.py
@@ -0,0 +1,54 @@
+from subprocess import Popen, PIPE
+import os
+import time
+import cv2
+from utils import get_framerate, print_video_size_info, generate_ffmpeg_cmd
+
+'''manual inputs'''
+input_path_relative = "assets/in.avi" # todo add video file to project folder and update filename
+output_path_relative = "assets/out.avi" # todo choose filename for output
+use_nvidia_gpu = False # todo set True if computer has a nvidia gpu and the correct sdk
+
+
+'''setup'''
+images = [] # contains the images after splitting
+project_dir = os.path.dirname(os.getcwd())
+input_path = os.path.join(project_dir, input_path_relative) # getting the absolute paths to input and output files
+output_path = os.path.join(project_dir, output_path_relative)
+fps = get_framerate(input_path) # getting framerate of original video
+
+
+'''splitting video into individual frames'''
+print("Splitting video...")
+start_time_split = time.time()
+capture = cv2.VideoCapture(input_path)
+
+while (True):
+    success, frame = capture.read() # reading induvidual frames from video
+    if success:
+        images.append(frame)
+    else:
+        break
+
+capture.release()
+cv2.destroyAllWindows()
+print(f"Time splitting: {round(time.time() - start_time_split, 4)}s")
+
+
+'''compressing the images in a video'''
+print("Compressing video...")
+height, width, _ = images[0].shape # getting the frame size
+start_time_compression = time.time()
+
+cmd = generate_ffmpeg_cmd(height, width, fps, output_path, use_nvidia_gpu)
+pipe = Popen(cmd, stdin=PIPE) # opening a pipe with ffmpeg
+
+for image in images: # writing all frames to the pipe
+    pipe.stdin.write(image.tobytes())
+
+pipe.stdin.close()
+pipe.wait() # wait for the ffmpeg process to finish
+
+print(f"Time compressing: {round(time.time() - start_time_compression, 4)}s")
+
+print_video_size_info(input_path, output_path)
---
+++ src/utils.py
@@ -0,0 +1,67 @@
+import cv2
+import os
+
+def get_framerate(path):
+    """
+    returns the framerate of the video on given path
+    :param path: absolute path to a video file
+    :return: framerate of video in fps
+    """
+    vidcap = cv2.VideoCapture(path)
+    fps = int(vidcap.get(cv2.CAP_PROP_FPS))
+    return fps
+
+
+def generate_ffmpeg_cmd(frame_width, frame_height, video_fps, output_path, use_nvidia_gpu = False):
+    """
+    generates the command to use ffmpeg
+    :param frame_width: number of pixels in the x-axis of the video
+    :param frame_height: number of pixels in the y-axis of the video
+    :param video_fps: desired framerate of the output video
+    :param output_path: absolute path including filename where the video will be saved to
+    :param use_nvidia_gpu: uses a nvidia compression codec for the compression
+    :return: command array
+    """
+    codec = "h264_nvenc" if use_nvidia_gpu else "h264"
+    return [
+        "ffmpeg",
+        "-loglevel", "error", # reduce console output by ffmpeg
+        "-y", # override output file if existent
+        "-an", # specify that video does not have sound
+        
+        # input
+        "-f", "rawvideo", # the input is undecoded
+        "-pix_fmt", "bgr24",
+        "-s", f"{frame_width}x{frame_height}", # needed as the input is raw video
+        "-r", str(video_fps),
+        "-i", "-", # needed as the frames are fed to ffmpeg through a pipe
+        
+        # output
+        "-vcodec", codec,
+        "-r", str(video_fps),
+        "-pix_fmt", "bgr24",
+        output_path
+    ]
+
+
+def print_video_size_info(path_original, path_compressed):
+    '''
+    prints the sizes of the original and compressed video as well as the compression ratio
+    :param path_original: absolute path to the uncompressed video file
+    :param path_compressed: absolute path to the uncompressed video file
+    :return: None
+    '''
+    def get_file_size(path):
+        '''
+         :param path: absolute path to the file
+         :return: file size in mb
+        '''
+        return os.stat(path).st_size / (1024 * 1024)
+
+    size_original_mb = get_file_size(path_original)
+    size_compressed_mb = get_file_size(path_compressed)
+
+    print(f"File size original: {round(size_original_mb, 4)} Mb")
+    print(f"File size compressed: {round(size_compressed_mb, 4)} Mb")
+    print(f"Compression ratio: {round(size_original_mb / size_compressed_mb, 4)}")
+
